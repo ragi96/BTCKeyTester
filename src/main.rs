cdd646aeb32de0d18d8e8879c6b9de4f97719d9c
ragi <ragi@github.com>
2023-02-04 15:04:13 +0100

    added address generation and goal address for stopping

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![allow(unused)]
-
+use bitcoin::network::constants::Network;
+use bitcoin::secp256k1::{Secp256k1, SecretKey};
+use bitcoin::util::address::Address;
+use bitcoin::util::base58;
+use bitcoin::util::key::KeyPair;
+use bitcoin::PublicKey;
 use clap::Parser;
-use hex::FromHex;
+use hex::{decode, FromHex};
 
-/// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     hex_key: String,
+    pub_key: String,
 }
+
 fn generate_combinations(hex_str: &str) -> Vec<String> {
     let hex_chars = vec![
         "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f",
@@ -42,12 +48,43 @@ fn generate_combinations(hex_str: &str) -> Vec<String> {
     combinations
 }
 
+fn private_key_to_p2pkh(private_key_hex: &str) -> Result<String, &'static str> {
+    let secp = Secp256k1::new();
+    // Convert hexadecimal private key string to bytes
+    let private_key_bytes = match hex::decode(private_key_hex) {
+        Ok(bytes) => bytes,
+        Err(_) => return Err("Invalid private key hexadecimal format"),
+    };
+
+    // Create secret key from bytes
+    let secret_key = match SecretKey::from_slice(&private_key_bytes) {
+        Ok(key) => key,
+        Err(_) => return Err("Invalid private key"),
+    };
+
+    let private_key = bitcoin::util::key::PrivateKey::new(secret_key, Network::Bitcoin);
+    let p2pkh_address =
+        Address::p2pkh(&private_key.public_key(&secp), Network::Bitcoin).to_string();
+    Ok(p2pkh_address)
+}
+
 fn main() {
     let args = Cli::parse();
     let hex_str = args.hex_key;
+    let pub_key = args.pub_key;
 
     let combinations = generate_combinations(&hex_str);
-    for c in combinations {
-        println!("{c}");
+
+    let pb = indicatif::ProgressBar::new(combinations.len() as u64);
+    let length = combinations.len();
+    for (mut counter, c) in combinations.into_iter().enumerate() {
+        counter += 1;
+        let p2pkh = private_key_to_p2pkh(&c).unwrap();
+        if p2pkh == pub_key {
+            println!("Found private key: {c}");
+            break;
+        }
+        pb.println(format!("[+] finished #{counter} of {length}"));
+        pb.inc(1)
     }
 }
